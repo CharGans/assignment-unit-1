f2bb51edea55ec4fdcae30b9219754cc02a48463
Charlotte Ganske <cganske04@icloud.com>
2024-11-12 11:09:58 -0600

    fixed js

---
+++ assignment/helloWorld.js
@@ -0,0 +1,28 @@
+console.log("Hello World!");
+
+//variables for info storage//
+//Strings//
+let firstname = 'charlotte';
+let petName = 'Shiso';
+let petType = 'Cat';
+let favoriteNumber = '8';
+
+//numbers (you can do math on numbers)//
+let temp = 25
+
+temp = temp - 20
+console.log(temp)
+
+//string concatenation//
+let aboutMyPet = petName + " is a " + petType;
+
+//boolean (only true or false)//
+let hungry = true
+
+//coditionals//
+
+if (hungry === true) {
+    console.log('go get dinner')
+} else {
+    console.log('time to code!')
+}
